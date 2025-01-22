2ac9b85f4d68792ba9e44db33d01b807d3eaf6e4
Rian Tri Mahendra <riantrimahendra@gmail.com>
2025-01-23 00:48:06 +0700

    Edit app.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './avatar.png';
 import './App.css';
 
 class App extends Component {
@@ -8,10 +8,10 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React!</h1>
+          <h1 className="App-title">Halo!</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Mari berkenalan, saya <a href="https://www.linkedin.com/in/riantm/">Rian</a>. Salam kenal!
         </p>
       </div>
     );
